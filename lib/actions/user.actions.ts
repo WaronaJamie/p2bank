2b2b954873c53c5c96c044c4c2c6103e25dc7770
WaronaJamie <jamiewarona@gmail.com>
2024-07-09 21:44:03 +0200

    Update user.actions.ts

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,17 +1,43 @@
-'use server';
+"use server";
 
 import { ID } from "node-appwrite";
 import { createAdminClient, createSessionClient } from "../apprwrite";
 import { cookies } from "next/headers";
 import { parseStringify } from "../utils";
 
-export const signIn = async ({ email, password }: signInProps) => {
-    
-    
+const {
+    APPWRITE_DATABASE_ID: DATABASE_ID,
+    APPWRITE_USER_COLLECTION_ID: USER_COLLECTION_ID,
+    APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID,
+  } = process.env;
+  
+  export const getUserInfo = async ({ userId }: getUserInfoProps) => {
     try {
-        const { account } = await createAdminClient();
+      const { database } = await createAdminClient();
+  
+      const user = await database.listDocuments(
+        DATABASE_ID!,
+        USER_COLLECTION_ID!,
+        [Query.equal('userId', [userId])]
+      )
+  
+      return parseStringify(user.documents[0]);
+    } catch (error) {
+      console.log(error)
+    }
+  }
 
+export const signIn = async ({ email, password }: signInProps) => {
+     try {
+        const { account } = await createAdminClient();
         const response = await account.createEmailPasswordSession(email, password);
+       
+        cookies().set("appwrite-session", session.secret, {
+            path: "/",
+            httpOnly: true,
+            sameSite: "strict",
+            secure: true,
+          });
 
         return parseStringify(response);
     } catch (error) {
